65be9e5c5840b17ad7243bb7e2d18324cb3e1bdc
Kienan Knight-Boehm <kienankb@gmail.com>
2014-09-15 13:12:40 -0400

    Added basic navigation drawer. Nonfunctional but present; swipe right from left edge of screen to view.

--- app/src/main/java/com/kienankb/selfsecured/selfsecured/MyActivity.java
+++ app/src/main/java/com/kienankb/selfsecured/selfsecured/MyActivity.java
@@ -2,16 +2,31 @@ package com.kienankb.selfsecured.selfsecured;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 public class MyActivity extends Activity {
 
+    private String[] mFeatureTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        //setContentView(R.layout.activity_my);
+        setContentView(R.layout.nav_drawer);
+        mFeatureTitles = getResources().getStringArray(R.array.featurelist);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_item,mFeatureTitles));
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener);
     }
 
 
