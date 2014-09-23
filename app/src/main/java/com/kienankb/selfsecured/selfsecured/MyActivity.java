1aef15a716543d1dce9bee2a84081211bfad5109
Kienan Knight-Boehm <kienankb@gmail.com>
2014-09-23 02:05:35 -0400

    Navigation drawer now opens/closes with tap on title.

--- app/src/main/java/com/kienankb/selfsecured/selfsecured/MyActivity.java
+++ app/src/main/java/com/kienankb/selfsecured/selfsecured/MyActivity.java
@@ -2,9 +2,11 @@ package com.kienankb.selfsecured.selfsecured;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,17 +18,37 @@ public class MyActivity extends Activity {
     private String[] mFeatureTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_my);
         setContentView(R.layout.nav_drawer);
         mFeatureTitles = getResources().getStringArray(R.array.featurelist);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_item,mFeatureTitles));
-        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener);
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+            this,
+            mDrawerLayout,
+            R.drawable.ic_drawer,
+            R.string.nav_open_title,
+            R.string.nav_close_title
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu();
+            }
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(R.string.nav_title);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
 
@@ -43,7 +65,7 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
